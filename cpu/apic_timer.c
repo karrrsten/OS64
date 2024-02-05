a8155e676ee6725007dacff7ee35a56ec80ad16f
Ben <benkarstens@web.de>
2024-02-05 17:04:57 +0100

    Improve apic_timer handler support

--- cpu/apic_timer.c
+++ cpu/apic_timer.c
@@ -5,23 +5,28 @@
 
 #include <cpuid.h>
 
+static void (*timer_handler)(struct interrupt_frame *frame);
+
+static void _timer_handler(struct interrupt_frame *frame) {
+	timer_handler(frame);
+	apic_eoi();
+}
+
 /**
  * @brief Set the Local APIC timer.
  * @param time The time in milliseconds.
- * @param handler An interrupt handler, to be registered for the timer. Note:
-the handler will need to call apic_eoi().
+ * @param handler An interrupt handler, to be registered for the timer.
  * @param mode The mode in which the timer is running. Can be
 APIC_TIMER_ONE_SHOT or APIC_TIMER_PERIODIC.
 */
-void apic_set_timer(uint32_t time, interrupt_handler handler, enum mode mode) {
+void apic_set_timer(uint32_t time, interrupt_handler handler,
+	enum apic_timer_mode mode) {
 	static bool did_init = false;
 	static uint8_t vector;
 	static uint64_t mHz_frequency;
 	if (!did_init) {
-		unsigned int eax, ebx, ecx, edx;
-		__get_cpuid(0x15, &eax, &ebx, &ecx, &edx);
-
-		mHz_frequency = ecx / 1000;
+		// TODO: find a portable way to determine frequency
+		mHz_frequency = 1000;
 
 		lapic_write(APIC_TIMER_DIVIDE, 0b1011);
 
@@ -29,9 +34,11 @@ void apic_set_timer(uint32_t time, interrupt_handler handler, enum mode mode) {
 		/* Disable the timer, enable the LVT entry */
 		lapic_write(APIC_TIMER_INIT, 0);
 		lapic_write(APIC_LVT_TIMER, (mode << 17) | vector);
+
+		idt_register(vector, _timer_handler);
 	}
 
-	idt_register(vector, handler);
+	timer_handler = handler;
 
 	/* Start the timer */
 	uint64_t count = time * mHz_frequency;
--- cpu/apic_timer.h
+++ cpu/apic_timer.h
@@ -4,11 +4,16 @@
 
 #include <stdint.h>
 
-enum mode {
+/**
+ * @enum apic_timer_mode
+ * @brief Modes for the APIC timer.
+ */
+enum apic_timer_mode {
 	APIC_TIMER_ONE_SHOT = 0b00,
 	APIC_TIMER_PERIODIC = 0b01
 };
 
-void apic_set_timer(uint32_t time, interrupt_handler handler, enum mode mode);
+void apic_set_timer(uint32_t time, interrupt_handler handler,
+	enum apic_timer_mode mode);
 void apic_mask_timer(void);
 void apic_unmask_timer(void);
