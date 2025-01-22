f7e079d0c92fe60b6fe18f1e87d0f7d2ce3ce8fe
Ben <benkarstens@web.de>
2025-01-22 18:36:33 +0100

    Initialize registers at boot

--- kernel/main.c
+++ kernel/main.c
@@ -20,6 +20,11 @@
  * @brief The main kernel function.
  */
 [[noreturn]] void kmain(void) {
+	wcr0(CR0_PG | CR0_WP | CR0_NE | CR0_ET | CR0_PE);
+	wcr4(CR4_PGE | CR4_PAE);
+	wrmsr(MSR_IA32_EFER,
+		IA32_EFER_NXE | IA32_EFER_LMA | IA32_EFER_LME | IA32_EFER_SCE);
+
 	serial_init();
 	kprintf("Initializing kernel...");
 
